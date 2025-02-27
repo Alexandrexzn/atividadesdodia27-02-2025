09ebad133fb7d89c449036636eb19fd0200d7497
Alexandrexzn <amorim.ismael.alexandre@gmail.com>
2025-02-27 17:17:04 -0300

    Add files via upload

---
+++ exercicio5.js
@@ -0,0 +1,9 @@
+let combustivel = 5.50
+let distancia = 500
+let consumo = 12
+
+function calculeConsumo(num1 , num2 , num3){
+   return (num2 / num3) * num1;
+}
+
+console.log(calculeConsumo(combustivel , distancia , consumo ));
